96bd447363c33251f8a1b29c03d6a2ef0552a487
Meisuleen Kased <meisuleen@gmail.com>
2019-04-10 03:21:08 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+  @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    end
